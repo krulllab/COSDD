ce0ff2e6351e76cc928536f6e225b8eb181048d7
Ben Salmon <brs209@student.bham.ac.uk>
2024-10-09 18:46:34 +0100

    load images with arbitrary axes

--- utils.py
+++ utils.py
@@ -1,11 +1,10 @@
 import random
 from pathlib import Path
-from typing import Callable, Literal
+from typing import Literal
 
 import torch
 import numpy as np
 from sklearn.linear_model import LinearRegression
-from skimage import io
 
 
 def patchify(x, patch_size):
@@ -14,10 +13,10 @@ def patchify(x, patch_size):
     If patch_size is not a divisor of the array shape, the edge patches will overlap
     with the adjacent patches.
     Args:
-        x (torch.Tensor): Array to patchify. Shape [B, C, Z | Y | X]
+        x (torch.Tensor): Array to patchify. Shape [S, C, Z | Y | X]
         patch_size (tuple): Size of the patches. Shape [z | y | x]
     Returns:
-        patches (torch.Tensor): Patchified array. Shape [B * n_patches, C, z | y | x]
+        patches (torch.Tensor): Patchified array. Shape [S * n_patches, C, z | y | x]
     """
     dimensions = x.ndim - 2
     assert (
@@ -27,7 +26,9 @@ def patchify(x, patch_size):
     remainders = [s % p for s, p in zip(x.shape[2:], patch_size)]
     remainder_dims = [i for i, r in enumerate(remainders) if r > 0]
     for d in remainder_dims:
-        pad = torch.narrow(x, d + 2, x.size(d + 2) - patch_size[d], patch_size[d]).contiguous()
+        pad = torch.narrow(
+            x, d + 2, x.size(d + 2) - patch_size[d], patch_size[d]
+        ).contiguous()
         x = torch.narrow(x, d + 2, 0, x.size(d + 2) - remainders[d]).contiguous()
         x = torch.cat([x, pad], dim=d + 2)
 
@@ -55,7 +56,9 @@ def autocorrelation(arr, max_lag=25):
     arr = arr - arr.mean()
     for i in range(max_lag):
         for j in range(max_lag):
-            c = (arr[..., :arr.shape[-2]-i, :arr.shape[-1]-j] * arr[..., i:, j:]).mean()
+            c = (
+                arr[..., : arr.shape[-2] - i, : arr.shape[-1] - j] * arr[..., i:, j:]
+            ).mean()
             covar[i, j] = c
 
     var = (arr**2).mean()
@@ -72,6 +75,7 @@ class RandomCrop:
     Args:
         output_size (tuple): Desired output size of the crop.
     """
+
     def __init__(self, output_size):
         self.output_size = output_size
         self.n_dims = len(output_size)
@@ -80,9 +84,11 @@ class RandomCrop:
         x_size = x.size()[1:]
         assert all(xs >= os for xs, os in zip(x_size, self.output_size))
 
-        start_idxs = [random.randint(0, xs - os) for xs, os in zip(x_size, self.output_size)]
+        start_idxs = [
+            random.randint(0, xs - os) for xs, os in zip(x_size, self.output_size)
+        ]
         end_idxs = [si + os for si, os in zip(start_idxs, self.output_size)]
-        crop = [slice(0, x.size(0))] 
+        crop = [slice(0, x.size(0))]
         crop += [slice(si, ei) for si, ei in zip(start_idxs, end_idxs)]
 
         return x[crop]
@@ -104,7 +110,7 @@ class TrainDataset(torch.utils.data.Dataset):
         if self.transform:
             image = self.transform(image)
         return image
-    
+
 
 class PredictDataset(torch.utils.data.Dataset):
 
@@ -136,17 +142,77 @@ def normalise(x):
     return x
 
 
+def get_defaults(config_dict):
+    defaults = {
+        "data": {
+            "paths": None,
+            "patterns": None,
+            "axes": None,
+            "number-dimensions": 2,
+            "patch-size": None,
+            "clip-outliers": False,
+        },
+        "train-params": {
+            "batch-size": 4,
+            "number-grad-batches": 4,
+            "crop-size": (256, 256),
+            "training-split": 0.9,
+            "max-epochs": 1000,
+            "patience": 100,
+        },
+        "hyper-parameters": {
+            "s-code-channels": 64,
+            "number-layers": 14,
+            "number-gaussians": 3,
+            "noise-direction": "x",
+            "direct-denoiser-loss": "MSE",
+        },
+        "use-direct-denoiser": True,
+        "model-name": None,
+        "precision": "bf16-mixed",
+        "checkpointed": True,
+        "gpus": [0],
+    }
+    for config in defaults:
+        if config not in config_dict.keys():
+            config_dict[config] = defaults[config]
+        elif isinstance(defaults[config], dict):
+            for subconfig in defaults[config]:
+                if subconfig not in config_dict[config].keys():
+                    config_dict[config][subconfig] = defaults[config][subconfig]
+    return config_dict
+
+
 def fix_axes(images, axes, n_dimensions):
     spatial_axes = [d for d in "TZYX" if d in axes]
     spatial_axes = spatial_axes[-n_dimensions:]
     sample_axes = [d for d in "STZYXC" if d in axes and d not in spatial_axes]
     target_axes = sample_axes + spatial_axes
     target_transpose = [axes.index(d) for d in target_axes]
+    missing_axes = [i for i in range(len(axes)) if i not in target_transpose]
+    target_transpose = missing_axes + target_transpose
     images = [i.transpose(target_transpose) for i in images]
     if "C" not in axes:
         images = [np.expand_dims(i, -n_dimensions - 1) for i in images]
-    images = [i.reshape(-1, i.shape[-n_dimensions - 1], *i.shape[-n_dimensions:]) for i in images]
+    images = [
+        i.reshape(-1, i.shape[-n_dimensions - 1], *i.shape[-n_dimensions:])
+        for i in images
+    ]
     return images
+
+
+def get_imread_fn(file_type):
+    if file_type in (".png", ".tif", ".tiff"):
+        from skimage import io
+
+        imread_fn = io.imread
+    elif file_type == ".czi":
+        import czifile
+
+        imread_fn = czifile.imread
+    else:
+        raise Exception(f"Unsupported file type: {file_type}")
+    return imread_fn
 
 
 """
@@ -183,6 +249,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from collections import deque
 
+
 def consume(iterator):
     deque(iterator, maxlen=0)
 
@@ -197,6 +264,10 @@ def _raise(e):
 def axes_check_and_normalize(axes, n_dimensions):
     """
     S(ample), T(ime), C(hannel), Z, Y, X
+
+    S(ample) can be repeated for extra dimensions that should be concatenated.
+    E.g., for data is stored as shape (3, 2, 1, 128, 1024, 1024, 4),
+    axes="SCSZYXS" returns images.shape=[3*1*4, 2, 128, 1024, 1024].
     """
     allowed = "STCZYX"
     axes is not None or _raise(ValueError("axis cannot be None."))
@@ -209,8 +280,10 @@ def axes_check_and_normalize(axes, n_dimensions):
         for a in axes
     )
     consume(
-        axes.count(a) == 1 or _raise(ValueError("axis '%s' occurs more than once." % a))
+        axes.count(a) == 1
+        or _raise(ValueError("axis '%s' cannot occur more than once." % a))
         for a in axes
+        if a != "S"
     )
     matches_n_dimensions = sum([a in "TZYX" for a in axes]) >= n_dimensions
     if not matches_n_dimensions:
@@ -225,7 +298,6 @@ def load_data(
     patterns: str | list = "*.tif",
     axes: str = "SYX",
     n_dimensions: Literal[1, 2, 3] = 2,
-    imread_fn: Callable[[str], np.ndarray] = None,
     dtype: torch.dtype = torch.float32,
 ):
     """Loads data from folders.
@@ -235,13 +307,10 @@ def load_data(
         patterns (str | list): Glob-style pattern to match images.
         axes (str): Semantics of axes of images as they are stored (assumed to be the same for all images).
         n_dimensions (int): Desired spatial dimensions of images once loaded (1, 2 or 3). Eg can be used to treat T(ime) as a spatial or sample dimension.
-        imread_fn (function): Function used to read images. Should return a numpy ndarray. Default works for common file types.
         dtype (np.dtype): Desired data type of loaded images.
     Returns:
         np.ndarray: The image data.
     """
-    if imread_fn is None:
-        imread_fn = io.imread
     if not isinstance(paths, list):
         paths = [paths]
     if not isinstance(patterns, list):
@@ -264,10 +333,16 @@ def load_data(
         for pattern in patterns:
             files.extend(list(path.glob(pattern)))
     len(files) != 0 or _raise(FileNotFoundError("Could not find any images"))
+    file_type = Path(files[0]).suffix
+    imread_fn = get_imread_fn(file_type)
     images = [imread_fn(f) for f in files]
     for i in images:
         if i.shape != images[0].shape:
-            _raise(ValueError(f"Images do not all have the same shape ({i.shape} and {images[0].shape})"))
+            _raise(
+                ValueError(
+                    f"Images do not all have the same shape ({i.shape} and {images[0].shape})"
+                )
+            )
     images[0].ndim == len(axes) or _raise(
         ValueError(f"Axes {axes} do not match shape of images: {images[0].shape}")
     )
